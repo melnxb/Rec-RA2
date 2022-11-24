00bba3be0a8a2e005bab1c4766baca6ab3e66b4e
melnxb <80221337+melnxb@users.noreply.github.com>
2022-11-23 22:57:30 -0300

    Add files via upload

---
+++ ##ex.1.py
@@ -0,0 +1,58 @@
+##ex.1
+#for i in range(5):
+alunos=[]
+nomes=[]
+valor=[]
+notas=[]
+
+
+
+for i in range(5):
+    nomes=str(input('EScreva um nome: '))
+    alunos.append(nomes)
+
+
+
+print(alunos)
+print(notas)
+
+for i in range(5):
+    valor=float(input('Digite 5 notas de 0.0 a 10.0, para cada aluno, respectivamente: '))
+    notas.append(valor)
+aluno1=
+
+
+for aluno in range(5):
+
+    if notas >= 7:
+         print(f'aprovado')
+    elif notas >= 4 and notas <7:
+         print('recuperação')
+    else:
+         print('reprovado')
+
+
+if notas >= 7:
+    print('aprovado')
+elif notas >= 4 and notas <7:
+    print('recuperação')
+else:
+    print('reprovado')
+nome=[]
+
+while True:
+    for i in range(5):
+        nomes=str(input('EScreva um nome: '))
+        alunos.append(nomes)
+        if not nome:
+            break
+        nota1=float(input('Digite 5 notas de 0.0 a 10.0, para cada aluno, respectivamente: '))
+        notas.append(valor)
+        
+
+    if notas >= 7:
+         print('aprovado')
+    elif notas >= 4 and notas <7:
+         print('recuperação')
+    else:
+         print('reprovado')
